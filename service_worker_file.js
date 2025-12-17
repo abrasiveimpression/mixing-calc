7a1f2263f68a21ee2b3eee3c0ead7a5714c0e390
abrasiveimpression <joshuamjones86@gmail.com>
2025-12-16 22:37:32 -0500

    Update service_worker_file.js

--- service_worker_file.js
+++ service_worker_file.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'mixing-calc-v1.4.0';
+const CACHE_NAME = 'mixing-calc-v1.5.0';
 const urlsToCache = [
     './',
     './index.html'
 ];
 // Install event - cache files
 self.addEventListener('install', (event) => {
-    console.log('Service Worker v1.4.0 installing...');
+    console.log('Service Worker v1.5.0 installing...');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
@@ -20,7 +20,7 @@ self.addEventListener('install', (event) => {
 });
 // Activate event - clean up old caches
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker v1.4.0 activating...');
+    console.log('Service Worker v1.5.0 activating...');
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
